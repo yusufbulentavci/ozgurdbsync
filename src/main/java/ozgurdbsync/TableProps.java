9fa870de1ac39240d37f74a06b84ea50522c2338
Y.Bulent Avci <avci.yusuf@gmail.com>
2020-11-03 11:51:22 +0300

    Eklenir

---
+++ src/main/java/ozgurdbsync/TableProps.java
@@ -0,0 +1,13 @@
+package ozgurdbsync;
+
+public class TableProps {
+	public TableProps(String schema, String table) {
+		super();
+		this.schema = schema;
+		this.table = table;
+	}
+	String schema;
+	String table;
+	
+
+}
